856c641d24c210f4dc748a9ebffd14a3acb3162a
Mocca Yang <tiffany12310@gmail.com>
2017-05-19 10:37:01 +0800

    loginView and add AppIcon pic

---
+++ photoWall/View/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  photoWall
+//
+//  Created by Mocca Yang on 2017/5/19.
+//  Copyright © 2017年 Appmocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
